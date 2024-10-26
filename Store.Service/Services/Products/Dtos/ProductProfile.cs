b63156b8f063c1f9abd20d7b1acf316aeca44222
mario emad <mario emad@LAPTOP-7A47C1CF>
2024-10-26 13:27:46 +0300

    Apply Auto Mapper

---
+++ Store.Service/Services/Products/Dtos/ProductProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.Services.Products.Dtos
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.BrandName, option => option.MapFrom(src=>src.Brand.Name))
+                .ForMember(dest => dest.TypeName, option => option.MapFrom(src=>src.Type.Name));
+        }
+    }
+}
--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Store.Data.Context;
 using Store.Repository.Interfaces;
 using Store.Repository.UnitOfWork;
+using Store.Service.Services.Products;
+using Store.Service.Services.Products.Dtos;
 using Store.Web.Helper;
 
 namespace Store.Web
@@ -19,11 +21,15 @@ namespace Store.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             builder.Services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DeafultConnection"));
             });
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddAutoMapper(typeof(ProductProfile));
 
             var app = builder.Build();
 
