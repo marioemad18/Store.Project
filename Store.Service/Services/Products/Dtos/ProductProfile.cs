8fae67cc385602174f28568e503615d1040b9d50
mario emad <mario emad@LAPTOP-7A47C1CF>
2024-10-26 14:29:35 +0300

    Fix Image Problem

--- Store.Service/Services/Products/Dtos/ProductProfile.cs
+++ Store.Service/Services/Products/Dtos/ProductProfile.cs
@@ -14,7 +14,8 @@ namespace Store.Service.Services.Products.Dtos
         {
             CreateMap<Product, ProductDto>()
                 .ForMember(dest => dest.BrandName, option => option.MapFrom(src=>src.Brand.Name))
-                .ForMember(dest => dest.TypeName, option => option.MapFrom(src=>src.Type.Name));
+                .ForMember(dest => dest.TypeName, option => option.MapFrom(src=>src.Type.Name))
+                .ForMember(dest => dest.ImageUrl, option => option.MapFrom<ProductImageResolver>());
 
             CreateMap<ProductBrand, BrandtypeDetailsDto>();
             CreateMap<ProductType, BrandtypeDetailsDto>();
